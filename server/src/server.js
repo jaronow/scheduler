1f9c85e0579d287f8076092f940334a1342eca44
jaronow <waronow123@gmail.com>
2019-08-15 13:13:01 -0700

    setup bare bones express server

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,24 @@
-console.log("testing testing!!")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 8081;
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+  res.send({
+    status: true,
+    message: "Welcome to the app"
+  });
+});
+
+app.listen(PORT, function(req, res) {
+  console.log(`App running on localhost: ${PORT}`);
+});
