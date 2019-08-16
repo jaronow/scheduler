c2674ba6fae2dc9c47fbf88d965bb3b77802a711
jaronow <waronow123@gmail.com>
2019-08-16 15:50:03 -0700

    tweaked navbar and added if statement to hide user if not logged in

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home-container">
-    <NavHead/>
     <h1>Home Page</h1>
     <Login/>
   </div>
 </template>
 
 <script>
-import NavHead from '@/components/NavHead'
 import Login from '@/components/Login'
 
 export default {
   name: "home",
   components: {
-    Login,
-    NavHead
+    Login
   }
 };
 </script>
