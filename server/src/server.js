e30308e6189da3d1fba1c4efddca5addb6eb1b05
jaronow <waronow123@gmail.com>
2019-08-15 17:53:42 -0700

    added path for login

--- server/src/server.js
+++ server/src/server.js
@@ -19,10 +19,17 @@ app.get('/', (req, res) => {
   });
 });
 
+app.post('/login', (req, res) => {
+  res.send({
+    status: true,
+    message: `${req.body.email} logged in`
+  });
+});
+
 app.post('/register', (req, res) => {
   res.send({
     status: true,
-    message: `User ${req.body.email} created!`
+    message: `User, ${req.body.email}, created!`
   });
 });
 
