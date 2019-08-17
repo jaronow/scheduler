c5c1c1d648cb6a3b28bebd526779438c43cca715
jaronow <waronow123@gmail.com>
2019-08-16 22:09:28 -0700

    created policy for checking if new user info is correct using Joi

---
+++ server/src/policies/authenticationControllerPolicy.js
@@ -0,0 +1,35 @@
+const Joi = require('joi');
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+    const {error, value} = Joi.validate(req.body, schema)
+    if (error) {
+      switch(error.details[0].context.key) {
+        case 'email': res.status(400).send({
+          error: 'Please provide a valid email address'
+        })
+        break;
+        case 'password': res.status(400).send({
+          error: `Provided password failed to match 1 or both rules:
+          <br>
+          1. Must contain the following characters: lowercase, uppercase, numbers. No spaces or special characters
+          <br>
+          2.Must be between 8 and 32 characters in length.`
+        })
+        break;
+        default: res.status(400).send({
+          error: 'Something went wrong, try again or contact us'
+          break;
+        })
+      }
+    } else {
+      next()
+    }
+  }
+};
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,7 @@
 const authentication = require('./controllers/authenticationController');
+const authenticationPolicy = require('./policies/authenticationControllerPolicy')
 
 module.exports = (app) => {
   app.post('/login', authentication.login);
-  app.post('/register', authentication.register);
+  app.post('/register', authenticationPolicy.register, authentication.register);
 };
