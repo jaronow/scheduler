2dad39cb5e61fb526b9c9da89fc6761d1b87d5c2
jaronow <waronow123@gmail.com>
2019-08-16 21:46:09 -0700

    made register user work with authentication controller

--- server/src/controllers/authenticationController.js
+++ server/src/controllers/authenticationController.js
@@ -1,3 +1,5 @@
+const {User} = require('../models')
+
 module.exports = {
   login (req, res) {
     res.send({
@@ -5,10 +7,14 @@ module.exports = {
       message: `${req.body.email} logged in`
     });
   },
-  register (req, res) {
-    res.send({
-      status: true,
-      message: `User, ${req.body.email}, created!`
-    });
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(400).send({
+        error: `${req.body.email} is already in use!`
+      })
+    }
   }
 };
